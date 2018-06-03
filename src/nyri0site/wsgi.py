8ea04539e1875af438247a0b0b361145a2ae6eef
Louis Sugy <louis.sugy@gmail.com>
2018-06-03 22:07:25 +0300

    Trying to solve 'module not found' problem

--- src/nyri0site/wsgi.py
+++ src/nyri0site/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                "../")))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nyri0site.settings")
 
