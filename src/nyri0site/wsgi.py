0528ec15c7fc4a5fa46d295e7d130ca79ef31a2b
Louis Sugy <louis.sugy@gmail.com>
2018-06-03 22:11:17 +0300

    Adding another folder to path

--- src/nyri0site/wsgi.py
+++ src/nyri0site/wsgi.py
@@ -14,6 +14,8 @@ from django.core.wsgi import get_wsgi_application
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
                                                 "../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                "../../")))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nyri0site.settings")
 
