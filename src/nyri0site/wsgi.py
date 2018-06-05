afc0e6c4a86862e907f100a609f0e354a536afb9
Louis Sugy <louis.sugy@gmail.com>
2018-06-06 00:37:11 +0300

    Trying to fix the path issue

--- src/nyri0site/wsgi.py
+++ src/nyri0site/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.insert(0, os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2]))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nyri0site.settings")
 
 application = get_wsgi_application()
