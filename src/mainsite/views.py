3de26e9a7a9d9ab40aa339c8ccbd1da6fb124239
Louis Sugy <louis.sugy@gmail.com>
2018-06-10 03:37:39 +0300

    Creating base templates and style

--- src/mainsite/views.py
+++ src/mainsite/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 
 def index(request):
-    return render(request, "index.html")
+    return HttpResponseRedirect("/about/")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def blog(request):
+    return render(request, "blog.html")
