2120ccb21c51616029ea9361096f6ecb8b181c29
Louis Sugy <louis.sugy@gmail.com>
2018-06-25 22:21:12 +0300

    Correcting handlerxxx parameters

--- src/mainsite/views.py
+++ src/mainsite/views.py
@@ -210,17 +210,25 @@ def confirm_email(request, key):
 
 # Error pages
 
-def handler404(request):
-    return render(request, '404.html', {})
+def handler404(request, *args, **kwargs):
+    response = render(request, '404.html', {})
+    response.status_code = 404
+    return response
 
 
-def handler500(request):
-    return render(request, '500.html', {})
+def handler500(request, *args, **kwargs):
+    response = render(request, '500.html', {})
+    response.status_code = 500
+    return response
 
 
-def handler403(request):
-    return render(request, '403.html', {})
+def handler403(request, *args, **kwargs):
+    response = render(request, '403.html', {})
+    response.status_code = 403
+    return response
 
 
-def handler400(request):
-    return render(request, '400.html', {})
+def handler400(request, *args, **kwargs):
+    response = render(request, '400.html', {})
+    response.status_code = 400
+    return response
